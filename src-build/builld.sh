a506e84816de273905af3d4576bdd894347d7de9
Dustin Getz <dustin.getz@gmail.com>
2025-01-23 16:44:31 -0500

    build.sh

---
+++ src-build/builld.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -eux -o pipefail
+
+clojure -T:build clean
+HYPERFIDDLE_HYPERFIDDLE_BUILD="v0-alpha-SNAPSHOT"
+clojure -T:build jar :version '"'$HYPERFIDDLE_HYPERFIDDLE_BUILD'"'
+clojure -T:build install :version '"'$HYPERFIDDLE_HYPERFIDDLE_BUILD'"'
+#env $(cat .env | xargs) clojure -T:build deploy :version '"'$HYPERFIDDLE_HYPERFIDDLE_BUILD'"'
