a5a1a0c19ca95d0f6031fc7828ddb74eb2992a8a
Geoffrey Gaillard <geoffrey.gaillard@hey.com>
2025-07-17 10:56:35 +0200

    [hyperfiddle] Import demos at build time or when used as a local dep
    User facing demo apps, like datomic-browser or hf-stater-app, contain demo code
    tuned for live-editing. We want demo code be the source of truth, with easy hot
    code reload, so code must stay in dedicated apps. But we also want to test those
    demos in electric fiddle. Instead of having duplicates in git or to depend on
    starter apps, the hyperfiddle lib will now copy demos when used as a
    `:local/root` dep, and now requires a `prep` stage: `clj -X:deps prep`. Copies
    won't be git-tracked. Demos will also be copied into the jar at build time.

--- src-build/build.clj
+++ src-build/build.clj
@@ -1,8 +1,27 @@
 (ns build
-  (:require [hyperfiddle.build :as build]))
+  (:require
+   [clojure.java.io :as io]
+   [clojure.tools.build.api :as tools.build]
+   [hyperfiddle.build :as build]))
 
 (def clean #'build/clean)
-(def build (partial build/build (build/create-basis :aliases [:release])))
 (def install #'build/install)
 (def deploy #'build/deploy)
 
+(defn import-demos! [& _]
+  (doseq [[demo-source-path demo-target-path] (:hyperfiddle/imported-demos (build/create-basis))]
+    (println "Importing" demo-source-path "->" demo-target-path)
+    (let [target (io/file demo-target-path)]
+      (when-not (.exists target)
+        (io/make-parents target)
+        (io/copy (io/file demo-source-path) target)))))
+
+(defn build
+  ([opts] (build (tools.build/create-basis) opts))
+  ([basis opts]
+   (clean basis opts)
+   (let [{:keys [class-dir src-dirs] :as opts} (build/defaults basis opts)]
+     (tools.build/write-pom opts)
+     (tools.build/copy-dir {:src-dirs src-dirs, :target-dir class-dir})
+     (import-demos!)
+     (tools.build/jar opts))))
